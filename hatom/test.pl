5d61033979d311ca1dbe242faae9d50baa4099d1
Robert Bachmann <rbachm@gmail.com>
2007-02-07 14:48:17 +0100

    test script: Added --dump option

--- hatom/test.pl
+++ hatom/test.pl
@@ -3,8 +3,7 @@ use strict;
 use FindBin; use File::Spec;
 use lib "$FindBin::Bin/../LIB/"; require XSLTest;
 
-chdir($FindBin::Bin);
-my $xslt = File::Spec->rel2abs('../hatom/hAtom2Atom.xsl');
+my $xslt = File::Spec->rel2abs('../hatom/hAtom2Atom.xsl', $FindBin::Bin);
 my $t = XSLTest::hAtom->new( {xslt_filename => $xslt} );
 $t->run();
 exit 0;
--- hcard/test.pl
+++ hcard/test.pl
@@ -3,8 +3,7 @@ use strict;
 use FindBin; use File::Spec;
 use lib "$FindBin::Bin/../LIB/"; require XSLTest;
 
-chdir($FindBin::Bin);
-my $xslt = File::Spec->rel2abs('./xhtml2vcard.xsl');
+my $xslt = File::Spec->rel2abs('./xhtml2vcard.xsl', $FindBin::Bin);
 my $t = XSLTest::hCard->new( {xslt_filename => $xslt} );
 $t->run();
 exit 0;
