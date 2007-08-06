46cffbfe1eb81fc5f071b6ef4440d5e76a854d54
Robert Bachmann <rbachm@gmail.com>
2007-08-06 21:23:26 +0200

    Backed out changeset 'XSLT test modules: 4XSLT, update for Win32'
    changset: decd993bcf932f5df26c2d839deeea7a15d1ec1d
    It is safer and faster to start 4XsltServer.py manually on Win32

--- LIB/XSLTest/Driver.pm
+++ LIB/XSLTest/Driver.pm
@@ -417,34 +417,26 @@ sub _load_saxon {
 
 sub _load_4xslt {
     my $self = shift;
-
-    $self->{console_out}->color_print( 
-        '---- Trying to spawn 4XsltServer.py ... ',
-        'teal'
-    );
-
-    my $script_path = $self->{lib_dir} . '4XsltServer.py';
-    my $r;
-
+    
     if ($^O !~ m/win32/i) {
-        $r = system($script_path, '--daemonize');
-    }
-    else {
-        $r = system('cmd.exe', '/c', 'start', '"4XsltServer"', 
-                    '/i', '/min', 'python.exe', $script_path);
-        sleep(1); #@@FIXME use "PID" file instead
-    }
-
-    if ($r == 0) {
+        $self->{console_out}->color_print( 
+            '---- Trying to spawn 4XsltServer.py ... ',
+            'teal'
+        );
+        
+        my $script_path = $self->{lib_dir} . '4XsltServer.py';
+        my $r = system($script_path, '--daemonize');
         $self->{'4xslt_daemon'} = 1;
-        $self->{console_out}->color_print('ok','lime');
-        print "\n";
+        
+        if ($r == 0) {
+            $self->{console_out}->color_print('ok','lime');
+            print "\n";
+        }
+        else {
+            $self->{console_out}->color_print('error','red');
+            print "\n";
+        }
     }
-    else {
-        $self->{console_out}->color_print('error','red');
-        print "\n";
-    }
-
 
     $self->{console_out}->color_print( 
         '---- Trying to connect to 4XsltServer.py ... ',
@@ -470,7 +462,7 @@ sub _load_4xslt {
     binmode($socket, ':utf8');
     $self->{'4xslt_socket'} = $socket;
 
-    $r = $self->_msg_4xslt('LOAD_XSLT '. $self->{xslt1_filename});
+    my $r = $self->_msg_4xslt('LOAD_XSLT '. $self->{xslt1_filename});
 
     if ($r !~ /^ok/i) {
         Carp::croak("Problem with 4XSLT: $r\n");
